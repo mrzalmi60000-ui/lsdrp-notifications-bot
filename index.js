eeb0620d57bd2ad118b73373b1a844413669aecf
mrzalmi60000-ui <mrzalmi60000@gmail.com>
2026-07-14 08:03:48 +0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`✅ ${client.user.tag} is online!`);
+});
+
+client.login(process.env.TOKEN);
